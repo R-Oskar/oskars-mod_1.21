c09f3504ceb347c19fbf76476a3228bab2d19b2b
Oskar <spamrakso@gmail.com>
2025-11-15 16:01:59 +0100

    added chisel with functionality

--- src/main/java/net/oskar/oskarsmod/item/ModItems.java
+++ src/main/java/net/oskar/oskarsmod/item/ModItems.java
@@ -7,11 +7,15 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.oskar.oskarsmod.OskarsMod;
+import net.oskar.oskarsmod.item.custom.ChiselItem;
 
 public class ModItems {
 
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(256)));
+
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(OskarsMod.MOD_ID, name), item);
     }
