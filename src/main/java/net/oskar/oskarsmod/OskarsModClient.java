728db4985e0113fe96b90253b13a8188d98df1ca
Oskar <spamrakso@gmail.com>
2025-11-15 09:59:42 +0100

    intial commit

---
+++ src/main/java/net/oskar/oskarsmod/OskarsModClient.java
@@ -0,0 +1,10 @@
+package net.oskar.oskarsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OskarsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/oskar/oskarsmod/OskarsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.oskar.oskarsmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OskarsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
